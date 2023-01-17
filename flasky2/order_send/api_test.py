434e7c9c98e846870134a580f84c1faa6ea442bd
ppd-Man <it.test@wiidreamthinker.com>
2023-01-17 20:35:41 +0800

    Create api_test.py

---
+++ flasky2/order_send/api_test.py
@@ -0,0 +1,10 @@
+﻿import requests
+
+url = "http://34.81.78.27/PayPayDrinkBackend/api/auth/login"
+
+payload = "{\r\n    \"account\":\"api\",\r\n    \"password\":\".iaKVMVf_8h_1i9y\"\r\n}"
+headers = {}
+
+response = requests.request("POST", url, headers=headers, data=payload)
+
+print(response.text)
