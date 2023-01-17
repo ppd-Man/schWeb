b4fca46b707cda41dadc8f43ac28fae4a4a4ae8f
ppd-Man <it.test@wiidreamthinker.com>
2023-01-17 20:34:49 +0800

    Create payconfiguration.py

---
+++ flasky2/payconfiguration.py
@@ -0,0 +1,18 @@
+﻿
+import os
+import sys
+def main():
+    print(os.name)
+MACHINE_IP=''
+WORK_DIR=''
+API_TOKEN = ''
+if os.uname()[0] == 'Linux' :
+    WORK_DIR='/home/pi/sch/schWeb/flasky2'    
+    MACHINE_IP='raspi2.local'
+    
+else:
+    WORK_DIR='/Users/alexfu/myPython/ebook-flask/schWeb/flasky2'
+    MACHINE_IP='192.168.50.75'
+    
+if __name__ == '__main__':
+    main()
